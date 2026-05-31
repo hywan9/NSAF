94cfea0df923df6ce38b4f47b951e91e5b8c6777
Haoyu Wang <hywan9@gmail.com>
2026-05-31 19:27:40 +0800

    update

--- scripts/NSAF.sh
+++ scripts/NSAF.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 model_name=NSAF
 
-for target in WS_1.5m WD_1.5m Ta_1.5m Ta_2.8m RH_1.5m RH_2.8m Vapor_1.5m Vapor_2.8m P RainSnow PAR Rn Ts_0cm Ts_20cm Ts_50cm Ts_100cm Ts_200cm Swc_0cm Swc_20cm Swc_50cm Swc_100cm Swc_200cm H LE Fc; do
+for target in Ta_1.5m Ta_2.8m RH_1.5m RH_2.8m Vapor_1.5m Vapor_2.8m P PAR Rn Ts_0cm Ts_20cm Ts_50cm Ts_100cm Ts_200cm Swc_0cm Swc_20cm Swc_50cm Swc_100cm Swc_200cm H LE Fc; do
   echo target: $target >>result_long_term_forecast.txt
 
   for pred_len in 96 192 336 720; do
